6cceb536058d83c398129a425762b3f3e9302f94
misteroh <madeline@done-digital.com>
2020-07-21 07:07:28 -0700

    adding back solutions

---
+++ solutions/blog_app_api/app/models/posts.rb
@@ -0,0 +1,3 @@
+class Posts < ApplicationRecord
+    belongs_to :users
+end
---
+++ solutions/blog_app_api/db/migrate/20200106021554_users.rb
@@ -0,0 +1,8 @@
+class Users < ActiveRecord::Migration[6.0]
+  def change 
+    create_table :users do |t|
+      t.string :name
+      t.string :password
+    end
+  end
+end
